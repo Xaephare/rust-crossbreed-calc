ba5b6e49dd88233b93c51bc2098db72ad4a95712
Louis Grace <louisgrace@onmail.com>
2023-04-08 22:00:06 +1200

    feat: add list prune

--- crossbreeder.py
+++ crossbreeder.py
@@ -1,4 +1,15 @@
+PRUNE_THRESHOLD = 3
+
 class CrossBreeder:
     def __init__(self):
         pass
-    
+
+    def prune(self, bit_vector_list):
+        '''Prunes a list of bit vectors based on a 'fitness' threshold'''
+        pruned_list = []
+        for plant in bit_vector_list:
+            fitness = plant[::3]
+            if fitness.count(1) <= PRUNE_THRESHOLD:
+                pruned_list.append(plant)
+
+        return pruned_list
--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import csv
 from bit_converter import BitConverter
+from crossbreeder import CrossBreeder
 
 
 plants = []
 
-with open('binary_plants.csv', 'r') as csvfile:
+with open('dataset.csv', 'r') as csvfile:
     data = csv.reader(csvfile)
     for row in data:
         plants.append(row)
 
 bit_vector_list = BitConverter().binary_plant(plants)
 
-with open('binary_plants.csv', 'w', newline='') as csvfile:
-    data = csv.writer(csvfile)
-    for plant in bit_vector_list:
-        data.writerow(plant)
+# with open('binary_plants.csv', 'w', newline='') as csvfile:
+#     data = csv.writer(csvfile)
+#     for plant in bit_vector_list:
+#         data.writerow(plant)
+
+crossbreeds = CrossBreeder().prune(bit_vector_list)
+print(BitConverter().string_plant(crossbreeds))
